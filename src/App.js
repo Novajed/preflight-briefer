03a7c9dab759ffc64e738cdae2614c5ec5dbef11
jrobb <robbinsware@gmail.com>
2021-01-10 20:16:20 -0500

    jr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,40 @@ import './App.css';
 const Api2Pdf = require('api2pdf');
 const a2pClient = new Api2Pdf(process.env.REACT_APP_PDF_API);
 
+
+
 class App extends Component {
+
+  render() {
+    const { result, showResult, merged, mergedDoc, crashHat, crashHelmet,value } = this.state;
+
+    return (
+      <div className="App">
+        <div>
+          <h1> PREFLIGHT <span className="turn-blue">BRIEFER</span>  </h1>
+          <p>We want to make it easy for pilots to create the documents they need.</p>
+          <p>Enter an ICAO airport code below and start creating your documents (Ex:"LAX"):</p>
+        </div>
+
+        <AircodeForm data={this.state.data} onChange={this.handleChange} value={value} />
+
+        <br />
+
+        <Button text="Generate PDF" onClick={this.generateDoc} />
+
+        <br />
+
+
+        {showResult ? <Results text={'Download'} resultLink={result} /> : null}
+        {merged ? <Results text={'Download All'} resultLink={mergedDoc} /> : null}
+        {crashHelmet ? <Results text={'Crash Me'} resultLink={crashHat} /> : null}
+
+      </div>
+    );
+  }
+
+
+
   constructor(props) {
     super(props);
 
@@ -20,16 +53,27 @@ class App extends Component {
       allDocs: [],
       mergedDoc: '',
       showResult: false,
+      crashHelmet: true,
       merged: false,
       data: [{ label: 'item 1' }, { label: 'item 2' }, { label: 'item 3' }],
     };
 
     this.toggle = this.toggle.bind(this);
     this.handleChange = this.handleChange.bind(this);
-
     this.generateDoc = this.generateDoc.bind(this);
     this.mergeAllDocs = this.mergeAllDocs.bind(this);
+    this.crashIt = this.crashIt.bind(this);
+
+
   }
+
+
+
+  showOptions() {
+    this.setState((prevState) => ({ showOptions: !prevState.showOptions }));
+  }
+
+
 
   toggle = (index, onChange) => {
     const newData = [...this.state.data];
@@ -37,27 +81,40 @@ class App extends Component {
       label: this.state.data[index].label,
       checked: !this.state.data[index].checked,
     });
+
     this.setState({ data: newData });
     onChange(newData.filter((x) => x.checked));
   };
+
+
 
   handleChange(e) {
     e.preventDefault();
     this.setState({ value: e.target.value });
   }
 
-  showOptions() {
-    this.setState((prevState) => ({ showOptions: !prevState.showOptions }));
-  }
+
+
+
+
+
+
+
 
   generateDoc() {
+
     // If user clicks before valid code entered, give error saying 'please enter valid aircode'
     this.setState({ showResult: true });
 
+
+
+
     const urls = [];
 
+    
+
+    // Create PDFs, save the links as variables, pass them to merge function
     trackPromise(
-      // Create PDFs, save the links as variables, pass them to merge function
       a2pClient
         .headlessChromeFromUrl(`https://www.airnav.com/airport/${this.state.aircode}`)
         .then((res) =>
@@ -66,6 +123,7 @@ class App extends Component {
           )
         )
     );
+
 
     trackPromise(
       // Create PDFs, save the links as variables, pass them to merge function
@@ -79,7 +137,13 @@ class App extends Component {
           )
         )
     );
+
+
   }
+
+
+
+
 
   mergeAllDocs() {
     trackPromise(
@@ -89,31 +153,23 @@ class App extends Component {
     );
   }
 
-  render() {
-    const { result, showResult, merged, mergedDoc, value } = this.state;
 
-    return (
-      <div className="App">
-        <div>
-          <h1>
-            PREFLIGHT <span className="turn-blue">BRIEFER</span>
-          </h1>
-          <p>We want to make it easy for pilots to create the documents they need.</p>
-          <p>Enter an ICAO airport code below and start creating your documents (Ex:"LAX"):</p>
-        </div>
 
-        <AircodeForm data={this.state.data} onChange={this.handleChange} value={value} />
 
-        <br />
 
-        <Button text="Generate PDF" onClick={this.generateDoc} />
+  crashIt() {
 
-        <br />
-        {showResult ? <Results text={'Download'} resultLink={result} /> : null}
-        {merged ? <Results text={'Download All'} resultLink={mergedDoc} /> : null}
-      </div>
-    );
+    
+    this.setState({ showResult: true });
+        // this.setState({ merged: true});
+    
+
+
   }
+
+
+
+  
 }
 
 export default App;
